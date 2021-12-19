4d9aab8964a03b920e87b45941edee9a3c207894
Michael Waterbury <mwaterbu94@gmail.com>
2021-12-19 15:26:23 -0500

    Speed up the parseFLUKAOutput.py script

--- parseFLUKAOutput.py
+++ parseFLUKAOutput.py
@@ -16,6 +16,16 @@ def pushFileToEvent(file):
             eNum = m[1]
             return line, eNum
 
+# Pushes all tracker files to next event
+def pushTrackersToNextEvent(trFiles, evNum):
+    evNum_tmp = evNum
+    while evNum_tmp == evNum:
+        trLines = []
+        for trFile in trFiles:
+            trLine, evNum_tmp = pushFileToEvent(trFile)
+            trLines.append(trLine)
+    return trLines
+
 # Reads the energy of the primary particle from the corresponding input file
 def pullPrimEn(fileID):
     inputFile = open(fileID[:-3] + '.inp')
@@ -69,49 +79,35 @@ def pullScData(f, line):
 
     return en
 
-def pullTrData(fileID,evNum):
-
-    files = [fileID + '_fort.' + str(fnum) for fnum in [30,31,32,33]]
-
-    trackers = []
-    for file in files:
-        with open(file, 'r') as f:
-            eNum = None
-            while not(eNum == evNum):
-                line = f.readline()
-                if re.search(r'Event',line):
-                    m = re.match(r'.*Event #: *([0-9]*).*',line)
-                    eNum = m[1]
-            m = re.match(r'.*tr([0-9]) .*Event #: *([0-9]*).*',line)
-            curTrack = int(m[1])
-
-            # Read whether bin was hit
-            line = f.readline()
-            m = re.match('.* ([0-9][0-9]*)',line)
-            numHits = int(m[1])
-            numLines = int(np.floor(numHits/2))
-            cells = []
-            flues = []
-            for n in range(numLines):
-                line = f.readline()
-                m = re.split('  *',line)
-                c1 = int(m[1])
-                fl1 = float(m[2])
-                c2 = int(m[3])
-                fl2 = float(m[4])
-                cells.append(c1)
-                cells.append(c2)
-                flues.append(fl1)
-                flues.append(fl2)
-            if (numHits % 2) == 1:
-                line = f.readline()
-                m = re.split('  *',line)
-                cl = int(m[1])
-                fl = float(m[2])
-                cells.append(cl)
-                flues.append(fl)
-        trackers.append([cells, flues])
-    return trackers
+# Reads the tracker data from the event line
+def pullTrData(f, line):
+    # Read number of hits in tracker
+    line = f.readline()
+    m = re.match('.* ([0-9][0-9]*)',line)
+    numHits = int(m[1])
+    # Determine how many lines need to be read for tracker
+    numLines = int(np.floor(numHits/2))
+    cells = []
+    flues = []
+    for n in range(numLines):
+        line = f.readline()
+        m = re.split('  *',line)
+        c1 = int(m[1])
+        fl1 = float(m[2])
+        c2 = int(m[3])
+        fl2 = float(m[4])
+        cells.append(c1)
+        cells.append(c2)
+        flues.append(fl1)
+        flues.append(fl2)
+    if (numHits % 2) == 1:
+        line = f.readline()
+        m = re.split('  *',line)
+        cl = int(m[1])
+        fl = float(m[2])
+        cells.append(cl)
+        flues.append(fl)
+    return [cells, flues]
 
 def reformatTracker(tracker, size=25):
     cells = tracker[0]
@@ -144,50 +140,58 @@ def scrapeFLUKA(history, entries, threshold, basedir, simid=''):
     #Scrape files
     ls = listdir(basedir)
     fileIDs = [basedir + re.match(r'(.*)_fort.24',fname)[1]
-                    for fname in ls if re.match(r'(.*)[0-9][0-9][0-9]_fort.24',fname)]
+        for fname in ls if re.match(r'(.*)[0-9][0-9][0-9]_fort.24',fname)]
     fileIDs.sort()
 
+    arraydir =  'ParsedData/'
     for fileID in fileIDs:
         scFile = open(fileID + '_fort.24')
+        tr1File = open(fileID + '_fort.30')
+        tr2File = open(fileID + '_fort.31')
+        tr3File = open(fileID + '_fort.32')
+        tr4File = open(fileID + '_fort.33')
+        trFiles = [tr1File, tr2File, tr3File, tr4File]
+
         fid = re.split('/',fileID)
         fid = fid[len(fid)-1]
-        arraydir =  'ParsedData/'
 
         primEn, totSim, zposition = pullSimData(fileID)
         if not(alreadyExists(arraydir, arraydir + simid + fid + '.npy')):
             print(fid)
             
             scLine, evNum = pushFileToEvent(scFile)
+            trLines = pushTrackersToNextEvent(trFiles, None) 
             evNum_tmp = evNum
             
             data = []
-            # if zposition > 120:
-            #     volLead = 30 * 40 * 2 # Volume of lead slice for neutrino interaction
-            #     rhoLead = 11.35 # Density of Lead
-            #     volTung = 25 * 30 * 8 # Volume of tungsten slice for neutrino interaction
-            #     rhoTung = 19.3 # Density of Tungsten
-            #     weight =  (rhoLead * volLead)/(volTung*rhoTung)
-            # else:
-            #     weight = 1
 
             while scLine:
 
+                # Pull Tracker Data
+                trackers = [pullTrData(trFile, trLine)
+                        for (trFile, trLine) in zip(trFiles, trLines)]
                 # Pull scintillator/calorimeter energy until you reach a new event
                 enHist   = []
                 while evNum == evNum_tmp:
                     energy = pullScData(scFile, scLine)
                     scLine, evNum_tmp = pushFileToEvent(scFile)
                     enHist.append(energy)
+                # Progress Tracker Files to Next Event
+                pushTrackersToNextEvent(trFiles, evNum)
+                # Reset evNum for next iteration
+                evNum = evNum_tmp
 
                 # print(enHist)
                 # Read data into self
                 enHist = np.array(enHist)
-                scintillator = np.array([int(value > threshold) for value in enHist])
+                scintillator = np.array([int(value > threshold) 
+                                            for value in enHist])
+                if re.match(r'.*_center.*',fid):
+                        scintillator[0] = 0
+                        scintillator[1] = 0
                 if np.equal(scintillator[entries], history).all():
                 # if np.equal(evHist, history).all() and enHist[9] > 10:
                     calEnergy = enHist[len(enHist)-1]
-                    trackers = pullTrData(fileID,evNum)
-                    
 
                     eventdata = {}
                     eventdata['primaryEnergy']       = primEn
@@ -201,8 +205,6 @@ def scrapeFLUKA(history, entries, threshold, basedir, simid=''):
                     eventdata['hits4']        = reformatTracker(trackers[3])
 
                     data.append(eventdata)
-                # Reset evNum to continue running reader
-                evNum = evNum_tmp
                 # print('----------------')
             np.save(arraydir + simid + fid + '.npy',data)
 
@@ -212,5 +214,5 @@ threshold = 10**-4
 
 basedir = 'GeneratedFiles/'
 
-scrapeFLUKA(history, entries, threshold, basedir, simid='_f_')
+scrapeFLUKA(history, entries, threshold, basedir, simid='')
 
